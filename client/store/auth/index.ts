00f31a96eb734dbfc1ce2d6c037b666851ec5812
EzioAuditore12 <dakshpurohit2005@gmail.com>
2025-12-28 23:43:24 +0530

    -a

--- client/store/auth/index.ts
+++ client/store/auth/index.ts
@@ -4,6 +4,8 @@ import { persist, createJSONStorage } from 'zustand/middleware';
 import type { AuthStoreType } from './type';
 import { zustandStorage } from '../storage';
 
+import { database } from '@/db';
+
 export const useAuthStore = create<AuthStoreType>()(
   persist(
     (set) => ({
@@ -11,7 +13,13 @@ export const useAuthStore = create<AuthStoreType>()(
       tokens: null,
       setTokens: (data) => set({ tokens: data }),
       setUserDetail: (data) => set({ user: data }),
-      logout: () => set({ user: null, tokens: null }),
+      logout: async () => {
+        await database.write(async () => {
+          await database.unsafeResetDatabase();
+        });
+
+        set({ user: null, tokens: null });
+      },
     }),
     {
       name: 'client-auth-store',
